0262fd460dd25addc3bbb49a9a12f0f5fd4d1c4d
Filip Ginter <ginter@cs.utu.fi>
2016-05-27 16:52:16 +0300

    run.sh produces commands which can be fed to gnu parallel

--- run.sh
+++ run.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 #countries=`cat nations_ready.txt | cut -f 1 | head -100`
-countries=`cat nations_ready.txt | cut -f 1 | tail -64`
+
+DATADIR="/home/jmnybl/git_checkout/maastereotypiat"
+
+countries=$(cat $DATADIR/nations_ready.txt | cut -f 1 | tail -64)
 
 for c in $countries
 do
-    echo $c
+    echo "#$c" > /dev/stderr
     if [ "$c" = "FI" ]
     then
-        zcat nationality_data_s24.conllu.gz | python3 significant_features.py $c 20 > results/s24_allwords_$c &
-        zcat nationality_data_pb.conllu.gz | python3 significant_features.py $c 20 > results/pb_allwords_$c &
-        wait
+        echo "zcat $DATADIR/nationality_data_s24.conllu.gz | python3 significant_features.py $c 20 > results/s24_allwords_$c"
+        echo "zcat $DATADIR/nationality_data_pb.conllu.gz | python3 significant_features.py $c 20 > results/pb_allwords_$c"
     else
-        zcat nationality_data_s24.conllu.gz | python3 remove_country_code.py FI | python3 significant_features.py $c 20 > results/s24_allwords_$c &
-        zcat nationality_data_pb.conllu.gz | python3 remove_country_code.py FI | python3 significant_features.py $c 20 > results/pb_allwords_$c &
-        wait
+        echo "zcat $DATADIR/nationality_data_s24.conllu.gz | python3 remove_country_code.py FI | python3 significant_features.py $c 20 > results/s24_allwords_$c"
+        echo "zcat $DATADIR/nationality_data_pb.conllu.gz | python3 remove_country_code.py FI | python3 significant_features.py $c 20 > results/pb_allwords_$c"
     fi
-    
 done
+echo "#Pipe me to | parallel -j 24" > /dev/stderr
